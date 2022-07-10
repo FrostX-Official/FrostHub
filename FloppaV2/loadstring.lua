1c7194682f0dc507b7cf0e0e0585aab11c311240
FrostX <76787958+FrostX-Official@users.noreply.github.com>
2022-07-10 15:03:29 +0300

    renamed dir to FrostHub

--- FloppaV2/loadstring.lua
+++ FloppaV2/loadstring.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/FrostX-Official/roblox-scripts/main/FloppaV2/main.lua"), true)()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/FrostX-Official/FrostHub/main/FloppaV2/main.lua"), true)()
